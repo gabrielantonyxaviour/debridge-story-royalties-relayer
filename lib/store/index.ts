865cabb1c1e7503dc731d0059d7e260eac9ccb99
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-03-17 12:32:17 +0530

    build fix

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -1,12 +1,10 @@
 // lib/store/index.ts
 import { createStore } from "zustand";
 import { createGlobalSlice, GlobalSlice } from "./globalSlice";
-import { createUserProfileSlice, UserProfileSlice } from "./userProfileSlice";
 
-export type EnvironmentStore = GlobalSlice & UserProfileSlice;
+export type EnvironmentStore = GlobalSlice;
 
 export const createEnvironmentStore = () =>
   createStore<EnvironmentStore>()((...a) => ({
     ...createGlobalSlice(...a),
-    ...createUserProfileSlice(...a),
   }));
